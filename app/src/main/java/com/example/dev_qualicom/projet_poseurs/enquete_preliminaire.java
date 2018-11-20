ab47ec2d5f755cd8869b41ed78f9e0c3f94d6a83
qualicomWendy <wendy@qualicom-conseil.fr>
2018-11-20 17:51:41 +0100

    wendy

---
+++ app/src/main/java/com/example/dev_qualicom/projet_poseurs/enquete_preliminaire.java
@@ -0,0 +1,13 @@
+package com.example.dev_qualicom.projet_poseurs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class enquete_preliminaire extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enquete_preliminaire);
+    }
+}
