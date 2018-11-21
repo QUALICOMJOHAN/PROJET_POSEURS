8dc10c3ecb6af36033afaacde8acde704e01365d
qualicomWendy <wendy@qualicom-conseil.fr>
2018-11-21 14:18:45 +0100

    wendy 2

---
+++ app/src/main/java/com/example/dev_qualicom/projet_poseurs/guide_inter.java
@@ -0,0 +1,13 @@
+package com.example.dev_qualicom.projet_poseurs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class guide_inter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guide_inter);
+    }
+}
