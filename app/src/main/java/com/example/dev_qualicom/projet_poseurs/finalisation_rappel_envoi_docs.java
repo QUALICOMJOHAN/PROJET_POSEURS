f7da5410c2a69735cf24bf256d3b388fc12a69a7
qualicomWendy <wendy@qualicom-conseil.fr>
2018-12-03 10:54:25 +0100

    wendy tab

---
+++ app/src/main/java/com/example/dev_qualicom/projet_poseurs/finalisation_rappel_envoi_docs.java
@@ -0,0 +1,13 @@
+package com.example.dev_qualicom.projet_poseurs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class finalisation_rappel_envoi_docs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_finalisation_rappel_envoi_docs);
+    }
+}
