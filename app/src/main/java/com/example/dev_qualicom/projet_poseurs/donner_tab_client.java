20be657c0a096f478a0d84095d0ff036667b2dce
qualicomWendy <wendy@qualicom-conseil.fr>
2018-12-06 09:10:49 +0100

    wendy 04/12

---
+++ app/src/main/java/com/example/dev_qualicom/projet_poseurs/donner_tab_client.java
@@ -0,0 +1,13 @@
+package com.example.dev_qualicom.projet_poseurs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class donner_tab_client extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donner_tab_client);
+    }
+}
