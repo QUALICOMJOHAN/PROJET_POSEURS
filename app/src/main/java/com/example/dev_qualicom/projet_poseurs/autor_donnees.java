e6c3afd8fe22e9337fc86fc6ec456b3aa2dd321a
qualicomWendy <wendy@qualicom-conseil.fr>
2018-12-07 14:34:52 +0100

    wendy 06/12 validey

---
+++ app/src/main/java/com/example/dev_qualicom/projet_poseurs/autor_donnees.java
@@ -0,0 +1,13 @@
+package com.example.dev_qualicom.projet_poseurs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class autor_donnees extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_autor_donnees);
+    }
+}
---
+++ app/src/main/java/com/example/dev_qualicom/projet_poseurs/finalisation_inter_import_thermique2.java
@@ -0,0 +1,13 @@
+package com.example.dev_qualicom.projet_poseurs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class finalisation_inter_import_thermique2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_finalisation_inter_import_thermique2);
+    }
+}
