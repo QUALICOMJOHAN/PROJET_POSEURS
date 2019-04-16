73ba3c0d60c0185c6fc28222982302051fe68fd5
qualicomWendy <wendy@qualicom-conseil.fr>
2019-04-16 10:17:42 +0200

    wendy 16/04 recycler view

---
+++ app/src/main/java/com/example/dev_qualicom/projet_poseurs/pdf_compte_rendu_pdc_item.java
@@ -0,0 +1,13 @@
+package com.example.dev_qualicom.projet_poseurs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class pdf_compte_rendu_pdc_item extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pdf_compte_rendu_pdc_item);
+    }
+}
